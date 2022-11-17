c7718e0fd45e7ea8bf8273e9bb7c56ef61c4ff96
Matt Daigle <Matttdaigle@gmail.com>
2022-11-17 09:41:49 -0500

    Moved all data into one file.

--- ScraperYellowPages/Business.cs
+++ ScraperYellowPages/Business.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ScraperYellowPages
+﻿namespace ScraperYellowPages
 {
     public class Business
     {
         public string Name { get; set; }
         public string Number { get; set; }
         public string Address { get; set; }
+        //public string Website { get; set; }
     }
 }
--- ScraperYellowPages/Program.cs
+++ ScraperYellowPages/Program.cs
@@ -2,6 +2,7 @@
 using HtmlAgilityPack;
 using ScrapySharp.Extensions;
 using ScrapySharp.Network;
+using System.ComponentModel;
 using System.Globalization;
 
 namespace ScraperYellowPages
@@ -10,18 +11,28 @@ namespace ScraperYellowPages
     class Program
     {
         static ScrapingBrowser _scrapingbrowser = new ScrapingBrowser();
-
         static void Main(string[] args)
         {
-            var scrapedData = GetBusinesses("https://api.rocketscrape.com/?apiKey=fed04a8c-59c3-4412-9173-90585a6df7d9&url=https://www.yellowpages.com/search?search_terms=cabinet&geo_location_terms=Orlando%2C%20FL&page=6");
-            ExportBusinessesToCsv(scrapedData);
+            List<Business> result = new List<Business>();
+            var pageNum = 1;
+            while (pageNum < 20)
+            {
+                var scrapedData = GetBusinesses("https://www.yellowpages.com/search?search_terms=Coffee%20Shops&geo_location_terms=Tallahassee%2C%20FL&page= " + pageNum++);
+                result.AddRange(scrapedData);
+            }
+            ExportBusinessesToCsv(result);
+        }
+
+        static HtmlNode GetHtml(string url)
+        {
+            WebPage webPage = _scrapingbrowser.NavigateToPage(new Uri(url));
+            return webPage.Html;
         }
         static List<Business> GetBusinesses(string url)
         {
             List<Business> result = new List<Business>();
             var html = GetHtml(url);
             var businesses = html.CssSelect(".v-card");
-
             foreach (var business in businesses)
             {
                 try
@@ -30,6 +41,19 @@ namespace ScraperYellowPages
                     var number = business.CssSelect(".phone");
                     var address = business.CssSelect(".adr");
 
+                    /*HtmlWeb web = new HtmlWeb();
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                    doc = web.Load(url);
+
+                    foreach(HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+                    {
+                        HtmlAttribute att = link.Attributes["href"];
+
+                        if (att.Value.Contains("href"))
+                        {
+                            Console.WriteLine(att.Value);
+                        }
+                    }*/
                     if (name.Count() == 1 && number.Count() == 1 && address.Count() == 1)
                     {
                         var newBusiness = new Business();
@@ -47,19 +71,14 @@ namespace ScraperYellowPages
             }
             return result;
         }
-        static void ExportBusinessesToCsv(List<Business> firstBusinessDetails)
+        static void ExportBusinessesToCsv(List<Business> result)
         {
             using (var writer = new StreamWriter($@"C:\Users\Xin\Dropbox\My PC (DESKTOP-E4HK43S)\Documents\WebScraper\Business Info_{DateTime.Now.ToFileTime()}.csv"))
             using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
             {
-                csv.WriteRecords(firstBusinessDetails);
 
+                csv.WriteRecords(result);
             }
-        }
-        static HtmlNode GetHtml(string url)
-        {
-            WebPage webPage = _scrapingbrowser.NavigateToPage(new Uri(url));
-            return webPage.Html;
         }
     }
 }
